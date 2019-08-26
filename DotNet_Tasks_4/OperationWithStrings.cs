9dca36a58582d21db96f21ebc46bcc2e602ebc46
Taras Kravchuk <tkravchuk6@gmail.com>
2019-08-26 15:42:25 +0300

    task 4

---
+++ DotNet_Tasks_4/OperationWithStrings.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Tasks_4
+{
+    public static class OperationWithStrings
+    {
+        public static List<string> Random()
+        {
+            List<string> randArr = new List<string>();
+            char[] letters = "QWERTYUIOPASDFGHJKLZXCVBNM".ToCharArray();
+
+            for (int k = 0; k < 120; k++)
+            {
+                Random r = new Random();
+                string random = "";
+                for (int i = 0; i < 4; i++)
+                {
+                    random += letters[r.Next(0, 25)].ToString();
+                    System.Threading.Thread.Sleep(1);
+                }
+                randArr.Add(random);
+            }
+            return randArr;
+        }
+
+        public static List<string> Find(List<string> arr)
+        {
+            List<string> newArr = new List<string>();
+            for (int i = 0; i < arr.Count; i++)
+            {   
+                for (int j = i + 1; j < arr.Count; j++)
+                {                    
+                        
+                        if (newArr.Count==0)
+                        {
+                            newArr.Add(arr[i]);
+                        }
+                        if (!(arr[i]==newArr[newArr.Count-1]))
+                        {
+                            if (!(arr[i].Substring(0,1)=="Z"))
+                            { newArr.Add(arr[i]); }
+                        }
+                }
+            }
+            return newArr;
+        }
+
+        public static List<string> Sort (List<string> arr)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                flag = false;
+                for (int i = 0; i < arr.Count - 1; ++i)
+                    if (arr[i].CompareTo(arr[i + 1]) > 0)
+                    {
+                        string buf = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = buf;
+                        flag = true;
+                    }
+            }
+            arr.Reverse();
+            return arr;
+        }
+
+        public static List<string> DisplayPage(List<string> arr, int pageNumber)
+        {
+            List<string> newArr = new List<string>();
+            return newArr;
+        }
+
+    }
+}
---
+++ DotNet_Tasks_4/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNet_Tasks_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1-2 p
+            Person person1 = new Person();
+            person1.Age = 18;
+            person1.Name = "Petro";
+            person1.PhoneNumbers = new List<string> { "280-230-15", "065-038-28-50", "555-66-00" };
+
+            Person person2 = new Person();
+            person2.Age = 25;
+            person2.Name = "Marta";
+            person2.PhoneNumbers = new List<string> { "244-48-10", "097-123-25-71", "444-23-58" };
+
+            Person person3 = new Person();
+            person3.Age = 20;
+            person3.Name = "Andriy";
+            person3.PhoneNumbers = new List<string> { "777-23-58", "068-258-66-72", "587-66-66" };
+
+            Person person4 = new Person();
+            person4.Age = 50;
+            person4.Name = "Yosyf";
+            person4.PhoneNumbers = new List<string> { "123-86-15", "068-563-54-25", "325-21-45" };
+
+            Person person5 = new Person();
+            person5.Age = 68;
+            person5.Name = "Vladimir";
+            person5.PhoneNumbers = new List<string> { "458-02-58", "086-541-58-52", "258-54-25" };
+
+            Person person6 = new Person();
+            person6.Age = 28;
+            person6.Name = "Liza";
+            person6.PhoneNumbers = new List<string> { "213-45-68", "098-558-55-21", "668-58-53" };
+
+            var list1 = new List<Person>();
+
+            list1.Add(person1);
+            list1.Add(person2);
+            list1.Add(person3);
+            list1.Add(person4);
+            list1.Add(person5);
+            list1.Add(person6);
+
+            Console.WriteLine("1. Name and age of all persons in List");
+            Console.WriteLine();
+
+            foreach (var item in list1)
+            {
+                Console.WriteLine(item.Age);
+                Console.WriteLine(item.Name);
+            }
+
+            Console.WriteLine();
+
+            Person person7 = new Person();
+            person7.Age = 35;
+            person7.Name = "Ivanna";
+            person7.PhoneNumbers = new List<string> { "153-12-58", "068-486-51-98", "588-42-79" };
+
+            Person person8 = new Person();
+            person8.Age = 15;
+            person8.Name = "Stepan";
+            person8.PhoneNumbers = new List<string> { "589-36-88", "050-888-69-25", "973-58-58" };
+
+            var list2 = new List<Person>();
+            list2.Add(person7);
+            list2.Add(person8);
+            
+            list1.AddRange(list2);
+
+            Console.WriteLine("2. Numbers of phones of all persons in List");
+            Console.WriteLine();
+
+            foreach (var item in list1)
+            {
+                foreach (var num in item.PhoneNumbers)
+                {
+                    Console.WriteLine(num);
+                }
+                Console.WriteLine();
+            }
+            #endregion
+
+            List<string> randArr = new List<string>();
+            randArr = OperationWithStrings.Random();
+
+            Console.WriteLine("3. List of random words");
+            Console.WriteLine();
+
+            foreach (var item in randArr)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+            Console.Write("Number of elements in List is: ");
+            Console.WriteLine(randArr.Count);
+            Console.WriteLine();
+
+            randArr = OperationWithStrings.Find(randArr);
+
+            Console.Write("Number of unqie elements in List is: ");
+            Console.WriteLine(randArr.Count);
+            Console.WriteLine();
+
+            randArr = OperationWithStrings.Sort(randArr);
+            Console.WriteLine();
+
+            
+        }
+    }
+}
